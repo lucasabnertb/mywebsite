62b6c9bdbd1f2b454036741988e9c7e0bd3773bd
alinegrazii <abner.lborges@hotmail.com>
2023-05-08 17:13:03 -0300

    alteracoes

--- contato.php
+++ contato.php
@@ -1,5 +1,7 @@
 <?php
 
+require './menu.php';
+
 //Funcao que verifica e executa o metodo POST
 if (isset($_POST['email']) && !empty($_POST['email'])) {
 
@@ -50,39 +52,12 @@ if (mail($email_destinatario,$subject,$body,$header)){
 
 <!DOCTYPE html>
 <html lang="pt-br">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <link rel="stylesheet" href="./styles/reset.css">
-  <link rel="stylesheet" href="./styles/main.css">
-  <link rel="stylesheet" href="./styles/form.css">
-  <link rel="icon" href="./styles/favicon-32x32.png">
-  <link rel="preconnect" href="https://fonts.googleapis.com">
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
-  <script src="https://kit.fontawesome.com/3ce734722c.js" crossorigin="anonymous"></script>
-
   <title>Projetos</title>
 </head>
 
-
-<body>
-  <header class="topo">
-    
-    <a href="./index.php"><h1><img class="logo" src="./imagens/logo1.png"></h1></a>
-   
-    <nav class="menu-nav">
-      <a class="menu" href="./index.php">Inicio</a>
-      <a class="menu" href="./projetos.php">Projetos</a>
-      <a class="menu" href="./sobre-mim.php">Sobre Mim</a>
-      <a class="menu" href="./contato.php">Contato</a>    
-      <a href="https://www.linkedin.com/in/lucasabnertb" target="_blank"><i class="fab fa-linkedin"></i></a>
-      <a href="https://github.com/lucasabnertb" target="_blank"><i class="fab fa-github"></i></a>
-    </nav>
-  </header>
+<body> 
   <main>
-    <div class="container">
-      
+    <div class="container">      
       <form class="form" method="POST" action="<echo">
 
         <label for="nome">Nome:</label>
@@ -98,7 +73,6 @@ if (mail($email_destinatario,$subject,$body,$header)){
         <textarea cols="30" rows="8" name='mensagem' class="input mensagem" required></textarea>
 
         <input type="submit" value="Enviar" class="button" />
-
       </form>
     </div> 
   </main>
---
+++ menu.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <link rel="stylesheet" href="./styles/reset.css">
+  <link rel="stylesheet" href="./styles/main.css">
+  <link rel="stylesheet" href="./styles/style.css">
+  <link rel="icon" href="./styles/favicon-32x32.png">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+  <script src="https://kit.fontawesome.com/3ce734722c.js" crossorigin="anonymous"></script>
+</head>
+
+
+  <header class="topo">
+    
+    <a href="./index.php"><h1><img class="logo" src="./imagens/logo1.png"></h1></a>
+   
+    <nav class="menu-nav">
+      <a class="menu" href="./index.php">Inicio</a>
+      <a class="menu" href="./projetos.php">Projetos</a>
+      <a class="menu" href="./sobre-mim.php">Sobre Mim</a>
+      <a class="menu" href="./contato.php">Contato</a>    
+      <a href="https://www.linkedin.com/in/lucasabnertb" target="_blank"><i class="fab fa-linkedin"></i></a>
+      <a href="https://github.com/lucasabnertb" target="_blank"><i class="fab fa-github"></i></a>
+    </nav>
+  </header>
