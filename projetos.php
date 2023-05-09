914a026d8174d06bd054aa10d64fb7cc5c9bb7c9
alinegrazii <abner.lborges@hotmail.com>
2023-05-09 15:38:16 -0300

    projetos

--- projetos.php
+++ projetos.php
@@ -8,9 +8,49 @@ require './menu.php';
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
+  <link rel="stylesheet" href="./styles/projetos.css">
   <title>Projetos</title>
 </head>
 
-
 <body>
+  <div class="caixa-lista">
+  <ul class="list">
+    <li class="projeto">
+      <a href="./projetos/calculadora_php/">
+        <img src="./projetos/calculadora_php/calculadora_php.jpg">
+          <h3>Calculadora</h3>
+      </a>
+    </li>
+    <li class="projeto">
+      <a href="./projetos/calculadora_php/">
+        <img src="./projetos/calculadora_php/calculadora_php.jpg">
+        <h3>Calculadora</h3>
+      </a>
+    </li>
+    <li class="projeto">
+      <a href="./projetos/calculadora_php/">
+        <img src="./projetos/calculadora_php/calculadora_php.jpg">
+        <h3>Calculadora</h3>
+      </a>
+    </li>
+    <li class="projeto">
+      <a href="./projetos/calculadora_php/">
+        <img src="./projetos/calculadora_php/calculadora_php.jpg">
+        <h3>Calculadora</h3>
+      </a>
+    </li>
+    <li class="projeto">
+      <a href="./projetos/calculadora_php/">
+        <img src="./projetos/calculadora_php/calculadora_php.jpg">
+        <h3>Calculadora</h3>
+      </a>
+    </li>
+    <li class="projeto">
+      <a href="./projetos/calculadora_php/">
+        <img src="./projetos/calculadora_php/calculadora_php.jpg">
+        <h3>Calculadora</h3>
+      </a>
+    </li>
+  </ul>
+  </div>
 </body>
