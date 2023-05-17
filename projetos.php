c641d5092748a57c56950cd130997cba1fe08b08
alinegrazii <abner.lborges@hotmail.com>
2023-05-17 18:27:32 -0300

    alteracoes

--- projetos.php
+++ projetos.php
@@ -32,7 +32,7 @@ require './menu.php';
           </a>
         </div>
         <div class="caixa-direita">
-          <h3>Calculadora</h3>
+          <h3>Consumo de API</h3>
           <p>Parágrafo é uma unidade autossuficiente de um discurso, na escrita, que lida com um ponto de vista ou ideia particular. Seu símbolo é §, apresentando a forma de dois esses entrelaçados, que representam as iniciais das palavras latinas signumsectionis. <p>
         </div>
       </li>
